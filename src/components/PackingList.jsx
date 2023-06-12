18fbb7d75c459e63f8de6df0557fe9c51eb049f0
gogi999 <g.petrovic9@gmail.com>
2023-06-12 17:33:30 +0200

    Added basic setup and started with Logo, Item, PackingList, Form and Stats components

---
+++ src/components/PackingList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { initialItems } from '../initialItems';
+import Item from './Item';
+
+const PackingList = () => {
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} key={item.id} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default PackingList;
